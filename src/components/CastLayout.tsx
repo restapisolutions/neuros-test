abfcbd30b64b9c0ee678acac22a13923063f0692
Peter Horvath <restapi.solutons@gmail.com>
2021-07-10 16:15:13 +0100

    Cast layout list with api data

---
+++ src/components/CastLayout.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import CastCard from "./CastCard";
+import styled from "styled-components";
+
+const CastContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-content: flex-start;
+  margin-left: 15px;
+  margin-right: 15px;
+  align-content: center;
+`;
+
+export default function CastLayout(props: any) {
+  function getCards() {
+    const data = props.tvShowAndCastData;
+    if (data !== undefined && data._embedded !== undefined) {
+      const cast = data._embedded.cast;
+      return cast.map((c: any) => {
+        return (
+          <CastCard
+            key={c.character.id}
+            name={c.person.name}
+            country={c.person.country.name}
+            birthday={c.person.birthday}
+            character={c.character.name}
+            img={c.character.image.medium}
+            gender={c.person.gender}
+          />
+        );
+      });
+    } else {
+      return <p>Loading</p>;
+    }
+  }
+
+  return <CastContainer>{getCards()}</CastContainer>;
+}
