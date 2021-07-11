6126c0e954a887d5f212b16c83f0ec4fb548d1ce
Peter Horvath <restapi.solutons@gmail.com>
2021-07-11 16:08:13 +0100

    Episodes layout

---
+++ src/components/EpisodesLayout.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import EpisodesCard from "./EpisodesCard";
+import styled from "styled-components";
+
+const EpisodesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-content: flex-start;
+  margin-left: 15px;
+  margin-right: 15px;
+  align-content: center;
+`;
+
+export default function EpisodesLayout(props: any) {
+  function getCards() {
+    const data = props.tvShowEpisodes;
+    if (data !== undefined) {
+      return data.map((ep: any) => {
+        return (
+          <EpisodesCard
+            key={ep.image.medium}
+            image={ep.image.medium}
+            name={ep.name}
+            description={ep.summary}
+            season={ep.season}
+            airtime={ep.airtime}
+          ></EpisodesCard>
+        );
+      });
+    }
+    return <p>Loading</p>;
+  }
+
+  return <EpisodesContainer>{getCards()}</EpisodesContainer>;
+}
